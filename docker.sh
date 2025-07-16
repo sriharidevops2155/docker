eebd1bae6180084672c8248a931ecda25702bfa2
sriharibandi <shreharii@gmail.com>
2025-07-16 21:43:52 +0530

    docker

--- docker.sh
+++ docker.sh
@@ -1,44 +1,15 @@
-resource "aws_instance" "docker" {
-  ami           = local.ami_id
-  #instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
-  instance_type = "t3.medium"
-  # need more for terraform
-  root_block_device {
-    volume_size = 50
-    volume_type = "gp3" # or "gp2", depending on your preference
-  }
-  user_data = file("docker.sh")
-  #iam_instance_profile = "TerraformAdmin"
-  tags = {
-     Name = "${var.project}-${var.environment}-docker"
-  }
-}
+#!/bin/bash
 
-resource "aws_security_group" "allow_all_docker" {
-    name        = "allow_all_docker"
-    description = "allow all traffic"
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
 
-    ingress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
-    egress {
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-        ipv6_cidr_blocks = ["::/0"]
-    }
+growpart /dev/nvme0n1 4
+lvextend -L +20G /dev/RootVG/rootVol
+lvextend -L +10G /dev/RootVG/varVol
 
-    lifecycle {
-      create_before_destroy = true
-    }
-
-    tags = {
-        Name = "allow-all-docker"
-    }
-}
+xfs_growfs /
+xfs_growfs /var
